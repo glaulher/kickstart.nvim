32d94c745904a28d2d94aba49ab9b1a69a35a745
glaulher <glaulher.developer@gmail.com>
2024-09-26 18:49:30 -0300

    feat: add sonarlint and reconfigure somethings

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -100,27 +100,6 @@ return {
       }
     end,
   },
-
-  -- require('sonarlint').setup {
-  --   server = {
-  --     cmd = {
-  --       'sonarlint-language-server',
-  --       -- Ensure that sonarlint-language-server uses stdio channel
-  --       '-stdio',
-  --       '-analyzers',
-  --       -- paths to the analyzers you need, using those for python and java in this example
-  --       vim.fn.expand '$MASON/share/sonarlint-analyzers/sonarpython.jar',
-  --       vim.fn.expand '$MASON/share/sonarlint-analyzers/sonarcfamily.jar',
-  --       vim.fn.expand '$MASON/share/sonarlint-analyzers/sonarjava.jar',
-  --     },
-  --   },
-  --   filetypes = {
-  --     -- Tested and working
-  --     'python',
-  --     'cpp',
-  --     'java',
-  --   },
-  -- },
 }
 --  (Código Unicode: U+F51E)
 --  (Código Unicode: U+F668)
--- lua/custom/plugins/keymappings.lua
+++ lua/custom/plugins/keymappings.lua
@@ -4,6 +4,7 @@ local keymap = vim.api.nvim_set_keymap
 -- Opções padrão (sem noremap, com silent)
 local opts = { noremap = true, silent = true }
 
+-- Movimentar a linha inteira
 -- Mapeamentos em modo Insert (insert_mode)
 keymap('i', '<A-j>', '<Esc>:m .+1<CR>==gi', opts)
 keymap('i', '<A-k>', '<Esc>:m .-2<CR>==gi', opts)
@@ -18,3 +19,4 @@ keymap('i', '<A-Right>', '<C-\\><C-N><C-w>l', opts)
 
 -- Retorna uma tabela vazia para evitar erro
 return {}
+
