d7da47329b00d56c26f7db3b260f31a0397c9c1a
glaulher <glaulher.developer@gmail.com>
2025-03-25 14:36:32 -0300

    feat:update delete lsp_format_opt

--- lua/core/plugins/autoformat-conform.lua
+++ lua/core/plugins/autoformat-conform.lua
@@ -20,7 +20,6 @@ return {
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
         local disable_filetypes = { c = true, cpp = true }
-        local lsp_format_opt
         if disable_filetypes[vim.bo[bufnr].filetype] then
           return nil
         else
