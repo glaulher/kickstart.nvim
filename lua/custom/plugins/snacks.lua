a3d31b59d62a26207678e3ac1e04e3b865cb274a
glaulher <glaulher.developer@gmail.com>
2025-07-17 12:52:38 -0300

    feat:focus effect by clicking the shortcut again

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -46,7 +46,26 @@ return {
       {
         '<leader>e',
         function()
-          Snacks.explorer()
+          local explorer_win = nil
+
+          for _, win in ipairs(vim.api.nvim_list_wins()) do
+            local buf = vim.api.nvim_win_get_buf(win)
+            local ft = vim.bo[buf].filetype
+            if ft == 'snacks_picker_list' then
+              explorer_win = win
+              break
+            end
+          end
+
+          if explorer_win then
+            if vim.api.nvim_get_current_win() == explorer_win then
+              vim.api.nvim_win_close(explorer_win, true)
+            else
+              vim.api.nvim_set_current_win(explorer_win)
+            end
+          else
+            Snacks.explorer()
+          end
         end,
         desc = 'Snacks File Explorer',
       },
