fa16b3bbe426426564d1dbcb66cd4f65f47e6fb0
glaulher <glaulher.developer@gmail.com>
2025-08-05 19:14:59 -0300

    feat:root path statusline

--- lua/kickstart/plugins/mini.lua
+++ lua/kickstart/plugins/mini.lua
@@ -1,28 +1,14 @@
 return {
   {
-    -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
     config = function()
-      -- Better Around/Inside textobjects
-      --
-      -- Examples:
-      --  - va)  - [V]isually select [A]round [)]paren
-      --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-      --  - ci'  - [C]hange [I]nside [']quote
       require('mini.ai').setup { n_lines = 500 }
-
-      -- Add/delete/replace surroundings (brackets, quotes, etc.)
-      --
-      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-      -- - sd'   - [S]urround [D]elete [']quotes
-      -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
 
-      -- Function to combine groups for statusline
       local function combine_groups(groups)
         local result = {}
         for _, group in ipairs(groups) do
-          if group.hl and group.strings then
+          if type(group) == 'table' and group.hl and group.strings then
             table.insert(result, '%#' .. group.hl .. '#')
             table.insert(result, table.concat(group.strings, ''))
           else
@@ -32,7 +18,6 @@ return {
         return table.concat(result, '')
       end
 
-      -- Helper function to create color highlights
       local make_color = function(hl_fg, hl_bg)
         local fghl = vim.api.nvim_get_hl(0, { name = hl_fg })
         local bghl = vim.api.nvim_get_hl(0, { name = hl_bg })
@@ -48,13 +33,37 @@ return {
         end
       end
 
-      -- Simple and easy statusline.
-      --  You could remove this setup call if you don't like it,
-      --  and try some other statusline plugin
-      local statusline = require 'mini.statusline'
-      -- set use_icons to true if you have a Nerd Font
-      statusline.setup {
+      local function get_project_root()
+        local bufnr = vim.api.nvim_get_current_buf()
+        local clients = vim.lsp.get_clients { bufnr = bufnr }
+        for _, client in ipairs(clients) do
+          local workspace_folders = client.config.workspace_folders
+          if workspace_folders and #workspace_folders > 0 then
+            local uri = workspace_folders[1].uri or workspace_folders[1]
+            local path = vim.uri_to_fname(uri)
+            return vim.fn.fnamemodify(path, ':p')
+          end
+          if client.config.root_dir then
+            return vim.fn.fnamemodify(client.config.root_dir, ':p')
+          end
+        end
 
+        local handle = io.popen 'git rev-parse --show-toplevel 2> /dev/null'
+        if handle then
+          local result = handle:read '*a'
+          handle:close()
+          result = vim.trim(result)
+          if result ~= '' then
+            return vim.fn.fnamemodify(result, ':p')
+          end
+        end
+
+        return vim.fn.getcwd()
+      end
+
+      local statusline = require 'mini.statusline'
+
+      statusline.setup {
         use_icons = vim.g.have_nerd_font,
         content = {
           active = function()
@@ -63,14 +72,29 @@ return {
             local diff = MiniStatusline.section_diff { trunc_width = 75 }
             local diagnostics = MiniStatusline.section_diagnostics { trunc_width = 75 }
             local lsp = MiniStatusline.section_lsp { trunc_width = 120 }
-            local filename = MiniStatusline.section_filename { trunc_width = 80 }
             local fileinfo = MiniStatusline.section_fileinfo { trunc_width = 100 }
-            local location = MiniStatusline.section_location { trunc_width = 75 }
+            local location = string.format('%2d:%-2d', vim.fn.line '.', vim.fn.col '.')
             local search = MiniStatusline.section_searchcount { trunc_width = 75 }
 
-            -- Get the current buffer number
+            local root = vim.fn.fnamemodify(get_project_root(), ':p'):gsub('/$', '')
+            local full_path = vim.fn.expand '%:p'
+            local display_path = ''
+            local project_name = vim.fn.fnamemodify(root, ':t')
+
+            if full_path == '' then
+              display_path = project_name .. '/[No Name]'
+            elseif full_path:sub(1, #root) == root then
+              local relative_path = full_path:sub(#root + 2)
+              if relative_path == '' then
+                display_path = project_name .. '/[No Name]'
+              else
+                display_path = project_name .. '/' .. relative_path
+              end
+            else
+              display_path = vim.fn.fnamemodify(full_path, ':.')
+            end
+
             local bufnr = vim.api.nvim_get_current_buf()
-            -- Get loaded LSP clients
             local lsp_clients = vim.lsp.get_clients { bufnr = bufnr }
             local lsp_names = {}
             for _, client in ipairs(lsp_clients) do
@@ -79,29 +103,32 @@ return {
 
             make_color(mode_hl, 'MiniStatuslineFilename')
             make_color('MiniStatuslineDevinfo', 'MiniStatuslineFilename')
-            make_color('MiniStatuslineFileInfo', 'MiniStatuslineFilename')
+            make_color('MiniStatuslineFileinfo', 'MiniStatuslineFilename')
 
             local tab = {
               { hl = mode_hl, strings = { mode } },
               { hl = mode_hl .. '2', strings = { '█ ' } },
-
               '%<',
             }
-            if table.concat({ lsp }):len() > 0 then
+
+            if lsp ~= '' or #lsp_names > 0 then
+              local combined = lsp
+              if #lsp_names > 0 then
+                combined = combined .. (lsp ~= '' and ' ' or '') .. table.concat(lsp_names, ', ')
+              end
               table.insert(tab, { hl = 'MiniStatuslineDevinfo2', strings = { '█' } })
-              table.insert(tab, { hl = 'MiniStatuslineDevinfo', strings = { lsp .. table.concat(lsp_names, ', ') } })
+              table.insert(tab, { hl = 'MiniStatuslineDevinfo', strings = { combined } })
               table.insert(tab, { hl = 'MiniStatuslineDevinfo2', strings = { '█ ' } })
-              -- table.insert(tab, '%<')
             end
 
-            if table.concat({ git, diff, diagnostics }):len() > 0 then
+            if (git .. diff .. diagnostics):len() > 0 then
               table.insert(tab, { hl = 'MiniStatuslineDevinfo2', strings = { '█' } })
-              table.insert(tab, { hl = 'MiniStatuslineDevinfo', strings = { git, ' ', diff, diagnostics } })
+              table.insert(tab, { hl = 'MiniStatuslineDevinfo', strings = { git, ' ', diff, ' ', diagnostics } })
               table.insert(tab, { hl = 'MiniStatuslineDevinfo2', strings = { '█' } })
               table.insert(tab, '%<')
             end
 
-            table.insert(tab, { hl = 'MiniStatuslineFilename', strings = { ' ', filename, ' ' } })
+            table.insert(tab, { hl = 'MiniStatuslineFilename', strings = { ' ', display_path, ' ' } })
             table.insert(tab, '%=')
 
             if fileinfo:len() > 0 then
@@ -113,23 +140,13 @@ return {
 
             table.insert(tab, { hl = mode_hl .. '2', strings = { '█' } })
             table.insert(tab, { hl = mode_hl, strings = { search, location } })
+
             return combine_groups(tab)
           end,
           inactive = nil,
         },
         set_vim_settings = true,
       }
-
-      -- You can configure sections in the statusline by overriding their
-      -- default behavior. For example, here we set the section for
-      -- cursor location to LINE:COLUMN
-      ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_location = function()
-        return '%2l:%-2v'
-      end
-
-      -- ... and there is more!
-      --  Check out: https://github.com/echasnovski/mini.nvim
     end,
   },
 }
